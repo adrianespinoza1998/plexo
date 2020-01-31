8b23ade90eff8e5dd7c91804eac25c1867518437
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2020-01-31 19:12:21 +0000

    fix crear proyecto

--- webservices/crear_proyecto.php
+++ webservices/crear_proyecto.php
@@ -11,84 +11,28 @@ if(isset($_POST['id_tipo_proyecto']) && isset($_POST['id_empresa']) && isset($_P
     $id_tipo_proyecto=$_POST['id_tipo_proyecto'];
     $id_empresa=$_POST['id_empresa'];
     $id_administrador=$_POST['id_administrador'];
-    $nombre_proyecto=$_POST['nombre_proyecto'];
+    $nombre_proyecto=strtolower($_POST['nombre_proyecto']);
     $link_proyecto=$_POST['link_proyecto'];
     $id_edificio=$_POST['id_edificio'];
 
-    $queryBuscarProyecto="SELECT nombre_proyecto FROM proyectos WHERE nombre_proyecto='".$nombre_proyecto."'";
+    $queryBuscarProyecto="SELECT nombre_proyecto FROM proyectos WHERE LOWER(nombre_proyecto)='".$nombre_proyecto."' AND id_empresa=".$id_empresa;
     $ejecutarBuscarProyectos=$conexion->query($queryBuscarProyecto);
 
-    if($ejecutarQuery->num_rows>0){
-        $row=$ejecutarBuscarProyectos->fetch_assoc();
-        if(substr($row['nombre_proyecto'],strlen($row['nombre_proyecto'])-1,1)==')' &&
-            substr($row['nombre_proyecto'],strlen($row['nombre_proyecto'])-3,1)=='('){
-            try{
-                $numero_repeticion=intval(substr($row['nombre_proyecto'],$row['nombre_proyecto']-3,1));
-                $nombreProyectoRepetido=substr($row['nombre_proyecto'],0,$row['nombre_proyecto']-4)."(".($numero_repeticion+1).")";
-
-                $query="INSERT INTO proyectos VALUES(0,".$id_tipo_proyecto.",".$id_empresa.",".$id_administrador.",'".$nombreProyectoRepetido.
-                    "','".$link_proyecto."',".$id_edificio.")";
-
-                $ejecutarQuery=$conexion->query($query);
-
-                if($ejecutarQuery==true){
-
-                    $salida['estado']='Proyecto creado';
-                    echo json_encode($salida);
-
-                }else{
-                    $salida['error']='error al insertar proyecto en bd';
-                    echo json_encode($salida);
-                }
-            }catch (Exception $e){
-                $nombreProyectoRepetido=substr($row['nombre_proyecto'],0,$row['nombre_proyecto']-4)."(1)";
-
-                $query="INSERT INTO proyectos VALUES(0,".$id_tipo_proyecto.",".$id_empresa.",".$id_administrador.",'".$nombreProyectoRepetido.
-                    "','".$link_proyecto."',".$id_edificio.")";
-
-                $ejecutarQuery=$conexion->query($query);
-
-                if($ejecutarQuery==true){
-
-                    $salida['estado']='Proyecto creado';
-                    echo json_encode($salida);
-
-                }else{
-                    $salida['error']='error al insertar proyecto en bd';
-                    echo json_encode($salida);
-                }
-            }
-        }else{
-            $nombreProyectoRepetido=substr($row['nombre_proyecto'],0,$row['nombre_proyecto']-4)."(1)";
-
-            $query="INSERT INTO proyectos VALUES(0,".$id_tipo_proyecto.",".$id_empresa.",".$id_administrador.",'".$nombreProyectoRepetido.
-                "','".$link_proyecto."',".$id_edificio.")";
-
-            $ejecutarQuery=$conexion->query($query);
-
-            if($ejecutarQuery==true){
-
-                $salida['estado']='Proyecto creado';
-                echo json_encode($salida);
-
-            }else{
-                $salida['error']='error al insertar proyecto en bd';
-                echo json_encode($salida);
-            }
-        }
+    if($ejecutarBuscarProyectos->num_rows>0){
+        $salida['error']="Nombre de proyecto ya existe";
+        echo json_encode($salida);
     }else{
-        $query="INSERT INTO proyectos VALUES(0,".$id_tipo_proyecto.",".$id_empresa.",".$id_administrador.",'".$nombre_proyecto.
-            "','".$link_proyecto."',".$id_edificio.")";
+
+        $query="INSERT INTO proyectos VALUES(0,".$id_tipo_proyecto.",".$id_empresa.",".$id_administrador.",'".$nombre_proyecto."','"
+            .$link_proyecto."',".$id_edificio.")";
 
         $ejecutarQuery=$conexion->query($query);
 
         if($ejecutarQuery==true){
-
-            $salida['estado']='Proyecto creado';
+            $salida['estado']="Proyecto creado";
             echo json_encode($salida);
-
         }else{
-            $salida['error']='error al insertar proyecto en bd';
+            $salida['error']="Error al insertar proyecto";
             echo json_encode($salida);
         }
     }
